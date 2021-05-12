861c71d436ee67364f58d7b46da4c4915b082190
erin <erinverkoulen93@gmail.com>
2021-05-12 17:46:25 -0500

    commit tree c16e817e8017b9fc71125c8178e9378e1cbbaa63

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = () => { 
+  let wordCaps = tutorials.map(element => element.split(' ').map(word => word[0].toUpperCase() + word.substring(1)).join(' '));
+return wordCaps;
+  
 }
